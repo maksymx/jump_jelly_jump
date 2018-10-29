798f2c02869cc53f4d39011bcbd7d01a22cf0d6e
Maksym <lynnykmaksym@gmail.com>
2018-10-29 19:32:55 +0200

    Updated due to deprecation of solution

--- Assets/Scripts/MenuStart.cs
+++ Assets/Scripts/MenuStart.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuStart : MonoBehaviour {
 
 	public void ChangeMenuScene(string SceneName){
-		Application.LoadLevel(SceneName);
+		SceneManager.LoadScene(SceneName);
 	}
 }
